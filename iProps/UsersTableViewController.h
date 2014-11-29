cb83469b0b21df81e98a8baa5746bb2e9e903bf1
Jakub Naliwajek <naliwajek@gmail.com>
2014-11-29 14:22:45 +0100

    Add files for UserTableViewController and refresh

---
+++ iProps/UsersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UsersTableViewController.h
+//  iProps
+//
+//  Created by netguru on 29/11/14.
+//  Copyright (c) 2014 Netguru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsersTableViewController : UITableViewController
+
+@end
