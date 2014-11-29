567a43631ca21787c70fb6e6af7ddf2f9d5443ea
Marek Buszman <mbusiq@gmail.com>
2014-11-29 12:44:51 +0100

    props table view :boom:

---
+++ iProps/PropsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PropsTableViewController.h
+//  iProps
+//
+//  Created by Marek Buszman on 29.11.2014.
+//  Copyright (c) 2014 Netguru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PropsTableViewController : UITableViewController
+
+@end
