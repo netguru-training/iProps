fbd4e981aeee4dddf90723546f453be77ece3ef0
Marek Buszman <mbusiq@gmail.com>
2014-11-29 13:17:56 +0100

    props table view is displaying something ;)

--- iProps/PropsTableViewController.m
+++ iProps/PropsTableViewController.m
@@ -34,24 +34,23 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 1;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"propsCell" forIndexPath:indexPath];
+  
+    cell.textLabel.text = @"Just kidding";
+  
     return cell;
 }
-*/
 
 /*
 // Override to support conditional editing of the table view.
