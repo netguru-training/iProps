8f4ebb1b0a1e3ff70284508b30cba2d4373d0c83
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-11-30 10:04:10 +0100

    add action for row tap

--- iProps/PropsTableViewController.m
+++ iProps/PropsTableViewController.m
@@ -96,6 +96,10 @@
     }
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"czesc %ld", indexPath.row);
+}
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
