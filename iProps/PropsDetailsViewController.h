d524c3ef715273cdd7a04f1fa226cbc2b7300e07
Jakub Naliwajek <naliwajek@gmail.com>
2014-11-30 09:40:58 +0100

    Add view for props details controller

---
+++ iProps/PropsDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  PropsDetailsViewController.h
+//  iProps
+//
+//  Created by netguru on 30/11/14.
+//  Copyright (c) 2014 Netguru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TweetModel.h"
+
+@interface PropsDetailsViewController : UIViewController
+
+@property (strong, nonatomic) TweetModel *props;
+
+@end
